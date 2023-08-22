04fa047eb6db8d0a68c95161058f687668ad905e
ilayda-simsek <c-isimsek@nexstar.tv>
2023-08-22 18:00:17 +0300

    Add benefits screen base

--- src/screens/yoga-practice/yoga-practice-details/components/benefits-screen.tsx
+++ src/screens/yoga-practice/yoga-practice-details/components/benefits-screen.tsx
@@ -1,21 +1,63 @@
 import React from 'react';
-import { StyleSheet, Text } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
-import { colors } from '@style';
+import { StyleSheet, Text, View } from 'react-native';
+import { typography, scale, colors } from '@style';
+import CircularProgress from 'react-native-circular-progress-indicator';
 
 const BenefitsScreen = () => {
   return (
-    <SafeAreaView style={styles.safeArea}>
-      <Text>BenefitsScreen</Text>
-    </SafeAreaView>
+    <View style={styles.safeArea}>
+      <Text style={[typography.h2, styles.header]}>Benefits</Text>
+      <Text style={[typography.p4, styles.body]}>
+        Cras orci orci, egestas eu aliquam ac, faucibus sed nulla nulla sit amet
+        orci vitae lectus bibendum tincidunt. Cras orci orci, egestas eu aliquam
+        ac, faucibus sed nulla nulla sit amet orci vitae lectus bibendum
+      </Text>
+      <Text style={[typography.p1, styles.subHeader]}>
+        Sequence Focus - Muscle Groups
+      </Text>
+
+      <View style={styles.musleGroupItem}>
+        <View>
+          <Text style={typography.h6}>Lower Back</Text>
+          <Text style={typography.p4}>Lorem ipsum dolor sit amet</Text>
+        </View>
+        <CircularProgress
+          progressValueColor={colors.darkPink}
+          activeStrokeColor={colors.darkPink}
+          inActiveStrokeColor={colors.backgroundGray}
+          activeStrokeWidth={5}
+          inActiveStrokeWidth={5}
+          value={50}
+          valueSuffix={'%'}
+          radius={22}
+        />
+      </View>
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
   safeArea: {
     flex: 1,
-    justifyContent: 'center',
-    backgroundColor: colors.backgroundGray,
+    padding: scale(14),
+  },
+  header: {
+    paddingBottom: scale(16),
+  },
+  subHeader: {
+    paddingBottom: scale(16),
+    color: colors.textGrayH2,
+  },
+  body: {
+    paddingBottom: scale(18),
+  },
+  musleGroupItem: {
+    borderRadius: 10,
+    backgroundColor: colors.white,
+    flexDirection: 'row',
+    alignItems: 'center',
+    padding: scale(12),
+    justifyContent: 'space-between',
   },
 });
 
