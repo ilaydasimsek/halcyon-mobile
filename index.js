b47e69f1a6f2f27f7bbbca78eaa1703a67ff4214
ilayda-simsek <c-isimsek@nexstar.tv>
2023-11-13 16:01:06 +0300

    Fix unnecessary playback listener

--- index.js
+++ index.js
@@ -5,8 +5,5 @@
 import { AppRegistry } from 'react-native';
 import App from './App';
 import { name as appName } from './app.json';
-import { playbackService } from './src/common/utils/track-player';
-import TrackPlayer from 'react-native-track-player';
 
 AppRegistry.registerComponent(appName, () => App);
-TrackPlayer.registerPlaybackService(() => playbackService);
