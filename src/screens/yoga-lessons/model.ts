144deda4e653a8baa332f7c0744552005ccf27ee
Onur Ari <onur@hellojasper.com>
2024-03-31 15:50:27 +0300

    Create article details page

--- src/screens/yoga-lessons/model.ts
+++ src/screens/yoga-lessons/model.ts
@@ -26,3 +26,23 @@ export type TYogaLesson = {
   coverImageUrl: string;
   steps: (TYogaLessonArticleStep | TYogaLessonPracticeStep)[];
 };
+
+type TArticleTextContentItem = {
+  __typename: 'ArticleTextContentItemNode';
+  content: string;
+};
+type TArticleImageContentItem = {
+  __typename: 'ArticleImageContentItemNode';
+  imageUrl: string;
+};
+
+export type TArticleContentItem =
+  | TArticleTextContentItem
+  | TArticleImageContentItem;
+
+export type TArticle = {
+  id: string;
+  title: string;
+  isPinned: boolean;
+  contentItems: TArticleContentItem[];
+};
